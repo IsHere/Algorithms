89bfd853c491e52df4d607e080e69e326d64e390
夏 <709177815@qq.com>
2018-08-16 18:39:13 +0800

    完善代理

--- src/algorithms/utils/ProxyUtil.java
+++ src/algorithms/utils/ProxyUtil.java
@@ -1,5 +1,19 @@
 package algorithms.utils;
 
+import algorithms.interfaces.GeneralSort;
+import algorithms.proxy.DynamicProxyTimeCaculateHandler;
+import algorithms.sort.QuickSort;
+
+import java.lang.reflect.Proxy;
+
 public class ProxyUtil {
 
+    public static void excuteSort(QuickSort sort, Integer[] a){
+        GeneralSort generalSortProxy =
+                (GeneralSort) Proxy.newProxyInstance(GeneralSort.class.getClassLoader(),
+                        new Class[]{GeneralSort.class},
+                        new DynamicProxyTimeCaculateHandler(sort));
+        generalSortProxy.sort(a);
+
+    }
 }
